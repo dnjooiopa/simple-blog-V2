9e711989d894b0d6974c5642db984c09bcc025c1
dnjooiopa <dnjooiopa@gmail.com>
2019-08-19 21:52:08 +0700

    add keys

--- app.js
+++ app.js
@@ -1,90 +1,94 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const _ = require('lodash');
+const mongoose = require('mongoose');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const _ = require("lodash");
-const mongoose = require("mongoose");
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent =
+   'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
+const aboutContent =
+   'Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.';
+const contactContent =
+   'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.';
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 //connect to atlas database
-const dbName = "blogDB"
-const uri = "mongodb://username:pasword@cluster0-shard-00-00-srznj.mongodb.net:27017,cluster0-shard-00-01-srznj.mongodb.net:27017,cluster0-shard-00-02-srznj.mongodb.net:27017/" + dbName + "?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+const dbName = 'blogDB';
+const uri = require('./config/keys').mongoURI;
 const urlLocal = "'mongodb://localhost:27017/blogDB";
 
-mongoose.connect(uri, { useNewUrlParser: true }).then(() => {
-  console.log("Successfully connected to atlas database.");
-}).catch((err) => console.error(err));
+mongoose
+   .connect(uri, { useNewUrlParser: true })
+   .then(() => {
+      console.log('Successfully connected to atlas database.');
+   })
+   .catch(err => console.error(err));
 
 const postSchema = {
-  title: String,
-  content: String
+   title: String,
+   content: String
 };
 
-const Post = mongoose.model("Post", postSchema);
+const Post = mongoose.model('Post', postSchema);
 
 //page
-app.get("/", function (req, res) {
-  Post.find(function (err, posts) {
-    res.render("home", {
-      startingContent: homeStartingContent,
-      posts: posts
-    });
-  });
+app.get('/', function(req, res) {
+   Post.find(function(err, posts) {
+      res.render('home', {
+         startingContent: homeStartingContent,
+         posts: posts
+      });
+   });
 });
 
-app.get("/about", function (req, res) {
-  res.render("about", { aboutContent: aboutContent });
+app.get('/about', function(req, res) {
+   res.render('about', { aboutContent: aboutContent });
 });
 
-app.get("/contact", function (req, res) {
-  res.render("contact", { contactContent: contactContent });
+app.get('/contact', function(req, res) {
+   res.render('contact', { contactContent: contactContent });
 });
 
-app.get("/compose", function (req, res) {
-  res.render("compose");
+app.get('/compose', function(req, res) {
+   res.render('compose');
 });
 
-app.post("/compose", function (req, res) {
-  const title = req.body.postTitle;
-  const content = req.body.postBody;
+app.post('/compose', function(req, res) {
+   const title = req.body.postTitle;
+   const content = req.body.postBody;
 
-  Post.findOne({ name: title }, function (err, foundPost) {
-    if (!foundPost) {
-      const newPost = new Post({
-        title: title,
-        content: content
+   Post.findOne({ name: title }, function(err, foundPost) {
+      if (!foundPost) {
+         const newPost = new Post({
+            title: title,
+            content: content
+         });
+         newPost.save();
+         res.redirect('/');
+      } else {
+         res.redirect('/');
       }
-      );
-      newPost.save();
-      res.redirect("/");
-    } else {
-      res.redirect("/");
-    }
-  })
+   });
 });
 
-app.get("/posts/:postID", function (req, res) {
-  const requestedID = _.capitalize(req.params.postID);
+app.get('/posts/:postID', function(req, res) {
+   const requestedID = _.capitalize(req.params.postID);
 
-  Post.findOne({ _id: requestedID }, function (err, foundPost) {
-    if (foundPost) {
-      res.render("post", { title: foundPost.title, content: foundPost.content });
-    } else {
-      res.render("notFound");
-    }
-  });
+   Post.findOne({ _id: requestedID }, function(err, foundPost) {
+      if (foundPost) {
+         res.render('post', { title: foundPost.title, content: foundPost.content });
+      } else {
+         res.render('notFound');
+      }
+   });
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(3000, function() {
+   console.log('Server started on port 3000');
 });
